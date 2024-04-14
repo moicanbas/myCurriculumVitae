4bddd4067c753d6f3093ac4a11c3c5bfafca5a6a
moises.cantillo <109709947+moicanbas@users.noreply.github.com>
2024-04-14 10:51:24 -0500

    update import components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve, dirname } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -19,7 +18,7 @@ export default defineConfig(() => {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
                 '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
-                '@components': fileURLToPath(new URL('./src/components', import.meta.url))
+                '@compone   nts': fileURLToPath(new URL('./src/components', import.meta.url))
             }
         },
         server: {
